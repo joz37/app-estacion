0af3525b6f97fcb693451ab8f094a45141704620
joz37 <joanferomero@gmail.com>
2023-10-20 19:08:40 -0300

    comienzo de la aplicacion app-clima

---
+++ js/main.js
@@ -0,0 +1,32 @@
+async function loadEstaciones(){
+	const response = await fetch("https://mattprofe.com.ar/proyectos/app-estacion/datos.php?mode=list-stations");
+	const data = await response.json();
+
+	return data;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	loadEstaciones().then( data => {
+		console.log(data);
+  
+ 		 data.forEach(info => {
+ 		 	createbtn(info)
+ 		 })
+
+	})
+})
+
+
+
+function createbtn(info){
+ 
+	const tpl = tpl_btn.content
+	const clon = tpl.cloneNode(true)
+
+	clon.querySelector(".card__estation").setAttribute("href", "./panel.php?chipid="+info.chipid)
+	clon.querySelector(".card__estation__header").innerHTML = info.apodo
+	clon.querySelector(".card__estation__ubication").innerHTML = info.ubicacion
+	clon.querySelector(".card__estation__views").innerHTML = info.visitas
+	listcont.appendChild(clon)
+
+}
